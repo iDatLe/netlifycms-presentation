2c2c9fe1f50c41f64d63f0d1733ffc7d9de3d1fc
iDatLe <LeDatDac@gmail.com>
2019-06-26 11:40:07 -0700

    Made index page map out blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,41 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link to={node.fields.slug}>
+            <h3>{node.frontmatter.tittle} </h3>
+            <p>{node.excerpt}</p>
+          </Link>
+        </div>
+      ))}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            date(formatString: "MMMM DD YYYY")
+            title
+          }
+          html
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
